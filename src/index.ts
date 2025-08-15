1dc4084c1adc2c1c32fcee05dd645df8a9c93912
Kurt <keduterte@gmail.com>
2025-08-16 01:12:42 +0800

    refactor: update exports in index.ts for better clarity and structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-export * from "./constants/http";
+export {
+  BAD_REQUEST,
+  CONFLICT,
+  CREATED,
+  FORBIDDEN,
+  HttpStatusCode,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND,
+  OK,
+  TOO_MANY_REQUESTS,
+  UNAUTHORIZED,
+  UNPROCESSABLE_CONTENT,
+} from "./constants/http";
 export { getDbSecrets } from "./db/getDbConnection";
 export { getDbConnection } from "./db/getDbConnection";
-export * from "./db/getPepper";
+export { getPepper } from "./db/getPepper";
